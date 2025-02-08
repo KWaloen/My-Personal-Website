cd597a6e1ea591b24ff274240b56f82be2edb2e4
KWaloen <k.waloen91@hotmail.com>
2025-02-08 19:18:55 +0100

    updated text

--- src/app/ReactProjects/LoginPage/IntroText.tsx
+++ src/app/ReactProjects/LoginPage/IntroText.tsx
@@ -4,7 +4,7 @@ export default function IntroductionText() {
         <div className = "card">
         
             <h1 className = "text">
-                This is a friByte CTF task. Can you find the environmental variables? <br/>
+                This is a friByte Capture the Flag task. Can you find the flag? <br/>
                 This is a little work in progress user registration and authentication log in page. This is connected to a MondoDB database. 
             </h1>
        
