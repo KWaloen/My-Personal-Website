9d982520f71cf20bb585021e96268ced1a0044af
KWaloen <k.waloen91@hotmail.com>
2025-03-31 00:43:39 +0200

    added link to baio

--- src/app/ReactProjects/React_Projects.tsx
+++ src/app/ReactProjects/React_Projects.tsx
@@ -7,15 +7,17 @@ export default function React_Projects() {
                 <h2 className="text-xl font-bold">React Projects</h2>
 
                 <h2>
-                    
+                    <ul>
+                        <Link className="link" href="https://baio.netlify.app/" target="_blank"> 💬 BAIO Bioinformatics Chatbot 💬</Link>
+                    </ul>
                     <ul>
                         <Link className="link" href="/ReactProjects/ToDoList"> ✔️ Todo list ✔️</Link>
                     </ul>
                     <ul>
-                        <Link className="link" href="/ReactProjects/LoginPage">🔑 Login Page 🔒</Link>
+                        <Link className="link" href="/ReactProjects/LoginPage">🔑 CTF Challenge: Login Page 🔒</Link>
                     </ul>
                     <ul>
-                        <Link className="link" href="/ReactProjects/Haskell_Is_Hard">👷 Haskell_Is_Hard.AI 👷</Link>
+                        <Link className="link" href="/ReactProjects/Haskell_Is_Hard">👷 Haskell_Is_Hard CHatbot 👷</Link>
                     </ul>
                 </h2>
 
